d7087a417e61182a542ce74566398365ee96794a
TheodaVidyani <theodahettiarachchi00@gmail.com>
2026-07-12 23:50:25 +0530

    X path written in Chem Material page.

--- src/main/java/com/ucsc/tutionplatform/pages/ChemistryMaterialPage.java
+++ src/main/java/com/ucsc/tutionplatform/pages/ChemistryMaterialPage.java
@@ -1,8 +1,14 @@
 package com.ucsc.tutionplatform.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ChemistryMaterialPage extends BasePage{
+
+    private By cloneFromSyllabus = By.xpath("//button[normalize-space()='Clone From Syllabus']");
+    private By saveMaterialsLibrary = By.xpath("//button[normalize-space()='Save Materials Library']");
+    private By reload = By.xpath("//button[normalize-space()='Reload']");
+
     // The constructor is mandatory to pass the driver to the BasePage
     public ChemistryMaterialPage(WebDriver driver) {
         super(driver);
